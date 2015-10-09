2a2666e4097eb4d80ffb75982438a9b8cd0ca57c
Victoria <vfilsaime@brandeis.edu>
2015-10-09 13:05:37 -0400

    tried to add pages lol

---
+++ client/messages.js
@@ -0,0 +1,43 @@
+Template.chat.events({
+	"submit #chatform": function(event){
+		
+		event.preventDefault();
+		
+		var name = $("#name") . val() ; 
+		console.log(name) ; 
+		
+		var major = $("#major") . val() ; 
+		console.log(name) ; 
+
+		var hometown = $("#hometown") . val() ; 
+		console.log(name) ; 
+		
+		
+		$("#chatinput").val("");
+
+		var profile = Meteor.user().profile;
+		
+		var chatline = 
+		  	{
+				uid:Meteor.userId(),  
+				who:profile["firstName"]+" "+profile["lastName"], 
+				name:name,
+				typyear:typyear,
+				hometown:hometown,
+				when: new Date()
+			};
+			
+		console.dir(chatline);
+		
+		ChatLines.insert(chatline);
+	}
+});
+
+Template.chat.helpers({
+	chatlines: function(){
+		return ChatLines.find({},{limit:10, sort:{when:-1}});
+	},
+	numchats: function(){
+		return ChatLines.find().count();
+	}
+});
