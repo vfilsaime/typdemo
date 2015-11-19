03ec98848c442bc956c86d9fee3369a3e9807329
William Tarimo <wtarimo@brandeis.edu>
2015-11-19 12:02:13 -0500

    Implemented NewsFeed

--- client/newsfeed.js
+++ client/newsfeed.js
@@ -1,74 +1,42 @@
-Template.newsfeed.events({
-	"submit #eventform": function(event){
+Template.newsPage.events({
+	"submit #newsform": function(event){
 		
 		event.preventDefault();
 		
-		var heading = $("#heading").val();
-		var date = $("#date_time").val();
-		var info = $("#info").val();
-		var location = $("#location").val();
+		var New_Post = $("#New_Post").val();
 		
-		$("#heading").val("");
-		$("#date_time").val("");
-		$("#info").val("");
-		$("#location").val("");
+		$("#New_Post").val("");
+		
 
 		var profile = Meteor.user().profile;
 		
-		var event = {
+		var news_item = {
 			uid:Meteor.userId(),  
-			name:profile["firstName"]+" "+profile["lastName"],
-			info:info,
-			heading:heading,
-			location:location,
-			attending:[],
-			when: date
+			who:profile["firstName"]+" "+profile["lastName"],
+			post:New_Post,
+			when: new Date()
 		};		
-		Events.insert(event);
+		News.insert(news_item);
 	}
 });
 
-Template.newsfeed.helpers({
-	events: function(){
-		return Events.find({},{sort:{when:-1}});
+Template.newsPage.helpers({
+	newsitems: function(){
+		return News.find({},{sort:{when:-1}});
 	},
-	numevents: function(){
-		return Events.find().count();
+	numposts: function(){
+		return News.find().count();
 	}
 });
 
 Template.news_item.helpers({
-	attending_count: function(){
-		return this.attending.length;
-	},
   	authorized: function(){
 	    return this.uid==Meteor.userId();
 	  }
 });
 
 Template.news_item.events({
-	"click #attending": function () {
-      var attending = this.attending;
-      var index = attending.indexOf(Meteor.userId());
-      if (index < 0) {
-      	attending.push(Meteor.userId());
-      }
-      Events.update(this._id, {
-  		$set: {attending:attending}
-  	  });
-    },
-
-	"click #notattending": function () {
-      var attending = this.attending;
-      index = attending.indexOf(Meteor.userId());
-      if (index > -1) {
-      	attending.splice(index, 1);
-      }
-      Events.update(this._id, {
-  		$set: {attending:attending}
-  	  });
-    },
     "click #delete": function () {
-    	Events.remove(this._id);
+    	News.remove(this._id);
     }
 });
