60a07f85d839e7ebf91ccfe764c55ff9851464f8
Victoria <vfilsaime@brandeis.edu>
2015-12-01 13:37:01 -0500

    updated stuff

--- server/server.js
+++ server/server.js
@@ -1,3 +1,6 @@
+Meteor.publish("users", function () {
+	return Meteor.users.find({}, {fields: {emails: 1, profile: 1, _id:1}});
+});
 
 Meteor.publish("users", function () {
 	return Meteor.users.find({}, {fields: {emails: 1, profile: 1, _id:1}});
